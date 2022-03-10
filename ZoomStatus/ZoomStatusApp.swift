dfc44107f783cdc61f744c9c30b788cfa544c02c
Jarod Lam <jarlam@deloitte.com.au>
2022-03-10 11:25:46 +1100

    Initial Commit

---
+++ ZoomStatus/ZoomStatusApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZoomStatusApp.swift
+//  ZoomStatus
+//
+//  Created by Lam, Jarod on 10/3/2022.
+//
+
+import SwiftUI
+
+@main
+struct ZoomStatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
